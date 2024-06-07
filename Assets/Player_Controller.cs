997fcfc6385ecfbb74634db5a54066a97f841d98
Bercik4200 <118359839+Bercik4200@users.noreply.github.com>
2024-06-07 18:05:53 +0200

    basic movement

---
+++ Assets/Player_Controller.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    private bool isGrounded;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        float move = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(move * moveSpeed, rb.velocity.y, 0);
+        rb.velocity = movement;
+    }
+
+    private void Jump()
+    {
+        if (isGrounded && Input.GetButtonDown("E"))
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
